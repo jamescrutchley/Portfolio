a00918258c5ccf720de2bff007fbdd45d8431d71
James Crutchley <jamescrutchley98@gmail.com>
2023-10-13 13:09:34 +1100

    Build header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,44 @@
 import styles from "../styles/Header.module.css";
-import React, { FunctionComponent } from 'react';
-
+import { Transition } from "../components/Transition";
 
 interface HeaderProps {
-    toggle: () => void;
-  }
-
+  toggle: () => void;
+}
 
 export const Header: FunctionComponent<HeaderProps> = ({ toggle }) => {
-
   return (
     <header className={styles.header}>
       <div className={styles.links}>
         <div className={styles.logo}>
-          <button className="darkTheme" onClick={() => toggle()}>
-
-          </button>
+          <button className="darkTheme" onClick={() => toggle()}></button>
+          <span className={styles.glow}></span>
         </div>
         <div className={styles.sidebar}>
           <ul>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="">Resume</a></li>
+
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="">Resume</a>
+            </li>
           </ul>
         </div>
       </div>
       <div className={styles.title}>
         <h1>James Crutchley</h1>
         <h2 className={styles.desc}>
-          I'm a Frontend <span>Dev</span>eloper with a passion for <span>dev</span>eloping my Web-<span>Dev</span>elopment abilities.
+          I'm a Frontend <span className={styles.special}>Dev</span>eloper with a passion for{" "}
+          <span className={styles.special}>dev</span>eloping well-<span className={styles.special}>Dev</span>eloped web applications.
         </h2>
       </div>
-      <div className={styles.transition}>
-        Scroll 
-      </div>
+      <Transition name="See More" next='about' />
     </header>
   );
 };
