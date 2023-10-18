c9bc5f620c308e47dd151170f9478b4414272245
James Crutchley <jamescrutchley98@gmail.com>
2023-10-18 11:53:33 +1100

    Add parsetext helper func

---
+++ src/helpers/parseText.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Portfolio.module.css'
+
+export const parseText = (copy) => {
+    // limit to 30 words.
+  let clipped = copy.split(" ").slice(0).join(" ");
+  clipped += `\n`;
+  const lines = clipped.split("\n");
+  return (
+    <div className={styles.projectDescription}>
+      {lines.map((line, index) => (
+        <span       className={`${styles.line} ${index === lines.length - 1 ? styles.lastLine : ''}`}
+        key={index}>{line}</span>
+      ))}
+    </div>
+  );
+};
