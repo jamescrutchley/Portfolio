bc9725aab5765bb3e5883211230dd994d193fbe2
James Crutchley <jamescrutchley98@gmail.com>
2023-10-25 13:33:53 +1100

    Build out contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,19 +7,35 @@ export const Contact = () => {
         <h3 className={styles.sectionHeader}>Contact</h3>
         <section className={styles.sectionContainer}>
           <div className={styles.email}>
-            <span className={styles.sectionLabel}>Email:</span>
-            <p>jamescrutchley98@gmail.com</p>
+            {/* <span className={styles.sectionLabel}>Email:</span> */}
+            <p>Email: jamescrutchley98@gmail.com</p>
+          </div>
+          <div className={styles.resumeRow}>
+            <a href="https://github.com/jamescrutchley">
+              <p>Github: @jamescrutchley</p>
+            </a>
+          </div>
+          <div className={styles.resumeRow}>
+            <a href="https://www.linkedin.com/in/james-crutchley-b16631190/">
+              <p>LinkedIn: @james-crutchley</p>
+            </a>
           </div>
         </section>
-        <h3 className={styles.sectionHeader}>Resume</h3>
-        <div className={styles.row}>
-          <p>Download: </p>
-          <a href="">(Link)</a>
-        </div>
-        <div className={styles.row}>
-          <p>Google Docs: </p>
-          <a href="">(Link)</a>
-        </div>
+        <section className={styles.sectionContainer}>
+          <h3 id="resume" className={styles.sectionHeader}>
+            Resume
+          </h3>
+          <div className={styles.resumeRow}>
+            <a href="">
+              <p>Download PDF (340KB)</p>
+            </a>
+          </div>
+          <div className={styles.resumeRow}>
+            <a href="">
+              <p>View on Google Docs</p>
+            </a>
+          </div>
+        </section>
       </div>
     </>
   );
