2e056eac6d4864b109f6eac14f8ff02d8f1b522e
James Crutchley <jamescrutchley98@gmail.com>
2023-10-25 13:34:18 +1100

    Build out project component

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,39 +1,49 @@
-import styles from '../../styles/Portfolio.module.css';
-import Carousel from './Carousel.tsx';
-import img from '../../assets/portfolio-images/shopping-cart-1.png';
-import { parseText } from '../../helpers/parseText.tsx';
-import {filterImages, produceImageCollection} from '../../helpers/filterImages.js';
-import {Link} from 'react-router-dom';
+import styles from "../../styles/Portfolio.module.css";
+import Carousel from "./Carousel.tsx";
+import img from "../../assets/portfolio-images/shopping-cart-1.png";
+import { parseText } from "../../helpers/parseText.tsx";
+import {
+  filterImages,
+  produceImageCollection,
+} from "../../helpers/filterImages.js";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function toKebabCase(inputString) {
-    return inputString.replace(/\s+/g, '-').toLowerCase();
+  return inputString.replace(/\s+/g, "-").toLowerCase();
 }
 
-const Project = ({title, description, details, images}) => {
-    const desc = parseText(description);
-    const detailUrls = filterImages(details);
-    const detailImages = produceImageCollection(detailUrls);
-    const titleString = toKebabCase(title)
+const Project = ({ title, description, tech, images, details, date }) => {
+  const desc = parseText(description);
+  const detailUrls = filterImages(tech);
+  const detailImages = produceImageCollection(detailUrls);
+  const titleString = toKebabCase(title);
 
-    return (
-        <li className={styles.project}>
-            <div className={styles.mask}></div>
-        <div className={styles.imageContainer}>
-                  {/* <img src={image} alt="project image" /> */}
-                  <Carousel images={images} />
-        </div>
-        <div className={styles.contentContainer}>
-          <h4>{title}</h4>
-          {<p>
-            {desc}
-          </p>}
-          <article className={styles.details}>{detailImages}</article>
-          <button className={styles.seeMore}>
-            <Link to={`project-page`} state={{ data: titleString }}> See more</Link>
-            </button>
-        </div>
-      </li>
-    )
-}
+  useEffect(() => console.log(title), [title]);
+
+  return (
+    <li className={styles.project}>
+      <div className={styles.mask}></div>
+      <div className={styles.mask}></div>
+      <div className={styles.imageContainer}>
+        {/* <img src={image} alt="project image" /> */}
+        <Carousel images={images} />
+      </div>
+      <div className={styles.contentContainer}>
+        <h4>{title}        <span className={styles.dateSpan}>{date}</span>
+</h4>
+        {<p>{desc}</p>}
+        <article className={styles.details}>{detailImages}</article>
+        <Link
+          to={title === 'React Design Patterns' ? `project/under-construction` : `project/${titleString}`}
+          state={{ data: title, details: details }}
+        >
+          {" "}
+          <button className={styles.seeMore}>See more </button>
+        </Link>
+      </div>
+    </li>
+  );
+};
 
 export default Project;
