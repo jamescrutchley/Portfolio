7356d482c6285b7d49b1ee13f6cb0ce36a1a2943
James Crutchley <jamescrutchley98@gmail.com>
2023-10-25 13:31:19 +1100

    Build out project page

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,71 +1,75 @@
 import styles from "../styles/ProjectPage.module.css";
 import img from "../assets/construction-site.svg";
-import {Link} from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import ScrollToTop from "../hooks/ScrollToTop";
+import { useEffect } from "react";
+import { parseText } from "../helpers/parseText";
+import maskStyle from '../styles/Portfolio.module.css'
 
+import Highlight from "react-highlight";
 
 export const ProjectPage = () => {
-    const location = useLocation();
-    const data = location.state.data; 
+  const location = useLocation();
+  const { data } = location.state;
+  const { details } = location.state;
+
+  useEffect(() => console.log(data), [data]);
+
+  const panels = Object.values(details).map((item, index) => {
+    console.log(item.img);
+    let urlString = item.img ? `/${item.img.toString()}` : null;
+
+    return (
+      <div key={index} className={styles.panel}>
+        {item.img && (
+          <div className={styles.imageSection}>
+            <img src={urlString} alt="project image" />
+          </div>
+        )}
+        {item.copy && (
+          <div className={styles.copySection}>
+            <p>{item.copy}</p>
+          </div>
+        )}
+        {item.code && (
+          <div className={styles.codeSection}>
+            <p className={styles.codeTitle}>{item.title}</p>
+            <Highlight className={styles.highlight} language="javascript">
+              {item.code}
+            </Highlight>
+          </div>
+        )}
+        {(item.website && item.github) && (
+            <div className={styles.buttonSection}>
+              <a href={item.website}><button>Project Website</button></a>
+              <a href={item.github}><button>Project Github Repo</button></a>
+            </div>
+        )}
+      </div>
+    );
+  });
 
   return (
     <>
-    <ScrollToTop />
-    <section className={styles.page}>
-      <header>
-      </header>
-      <div className={styles.container}>
-        <h1>{data}</h1>
+      <ScrollToTop />
+      <section className={styles.page}>
+        <header>
+          <h1>{data}</h1>
+        </header>
+        <div className={styles.container}>
+            <div className={styles.mask}></div>
+            <div className={styles.mask}></div>
+            <div className={styles.mask}></div>
+            <div className={styles.mask}></div>
 
-        <div className={styles.panel}>
-          <div className={styles.imageSection}>
-            <img src={img} alt="" />
-          </div>
-          <div className={styles.copySection}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis deserunt itaque animi, ullam perspiciatis dolorum eum,
-              magni praesentium rerum veniam dolorem tenetur nesciunt,
-              consequatur voluptatem magnam non. Dolores, vel nihil.
-            </p>
-          </div>
+          {panels}
+
         </div>
-        <div className={styles.panel}>
-          <div className={styles.imageSection}>
-            <img src={img} alt="" />
-          </div>
-          <div className={styles.copySection}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis deserunt itaque animi, ullam perspiciatis dolorum eum,
-              magni praesentium rerum veniam dolorem tenetur nesciunt,
-              consequatur voluptatem magnam non. Dolores, vel nihil.
-            </p>
-          </div>
-        </div>
-        <div className={styles.panel}>
-          <div className={styles.imageSection}>
-            <img src={img} alt="" />
-          </div>
-          <div className={styles.copySection}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis deserunt itaque animi, ullam perspiciatis dolorum eum,
-              magni praesentium rerum veniam dolorem tenetur nesciunt,
-              consequatur voluptatem magnam non. Dolores, vel nihil.
-            </p>
-          </div>
-        </div>
-        <div className={styles.buttonSection}>
-          <button>Project Website</button>
-          <button>Project Github Repo</button>
-        </div>
-      </div>
-      <button className={styles.backHome}>
-       <Link to="/">Back to Portfolio</Link>
-        </button>
-    </section>
+
+          <Link to="/"><button className={styles.backHome}>Back to Portfolio</button></Link>
+        
+      </section>
     </>
   );
 };
